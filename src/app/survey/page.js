75b3d6a7cb0bda03306f6b81b0d9962d8bb204aa
javademami <javademami2030@gmail.com>
2024-12-28 19:04:00 +0100

    Initial commit: Survey project setup

---
+++ src/app/survey/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SurveyForm from "../components/SurveyForm";
+
+const SurveyPage = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+        <SurveyForm />
+    </div>
+  );
+};
+
+export default SurveyPage;
